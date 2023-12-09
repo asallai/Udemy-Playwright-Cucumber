15a2a49d3cb264283b2435c74fce3e6bb24e6b27
Aniko Sallai <aniko.sallai@gmail.com>
2023-12-09 21:25:08 +0100

    92. Scenario Outlines

--- page-objects/login-page.js
+++ page-objects/login-page.js
@@ -10,8 +10,18 @@ class LoginPage {
         await page.click('#login-button')
     }
 
+    async submitLoginFormWithParameters(username, password) {
+        await page.fill('#user-name', username)
+        await page.fill('#password', password)
+        await page.click('#login-button')
+    }
+
     async assertUserIsLoggedIn() {
         await page.waitForSelector('.inventory_list')
+    }
+
+    async pause() {
+        await page.waitForTimeout(3000)
     }
 }
 
